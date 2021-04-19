0db7bc0bd5f2e0f6abeb0ad396a59ff98b81351f
devman-3000 <koliamarchuk50@gmail.com>
2021-04-19 17:34:54 +0300

    11th-laba

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
-
+use App\Http\Controllers\DiggingDeeperController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +21,13 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
+});
 
 Route::resource('rest', RestTestController::class)->names('restTest');
 
